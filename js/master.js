89c1aa26ff8ad09a06f8cf06dd6b122fa64ad5f3
Nick Van Hoogenstyn <nickvanhoog@gmail.com>
2013-02-18 23:46:59 -0800

    Separated JS from HTML, this took me fucking forever I hate my life

---
+++ js/master.js
@@ -0,0 +1,32 @@
+// Additional JS functions here
+console.log("WTF");
+window.fbAsyncInit = function() {
+    FB.init({
+          appId      : '148310075327662', // App ID
+          channelUrl : '//localhost:8080/channel.html', // Channel File
+          status     : true, // check login status
+          cookie     : true, // enable cookies to allow the server to access the session
+          xfbml      : true  // parse XFBML
+      });
+
+    FB.getLoginStatus(function(response){
+            //not logged in upon opening the page
+            if (response.status === 'connected') {
+                console.log("CONNECTED");
+                window.location = "player";
+            } 
+            else {
+                console.log("NOT CONNECTED");
+                window.location = "login";
+            }
+        });       
+};
+
+// Load the SDK Asynchronously
+(function(d){
+    var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement('script'); js.id = id; js.async = true;
+    js.src = "//connect.facebook.net/en_US/all.js";
+    ref.parentNode.insertBefore(js, ref);
+}(document));
