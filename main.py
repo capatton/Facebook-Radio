dfe76268e0eaca2ed040e3985ea1b0aeb22d758a
Nick Van Hoogenstyn <nickvanhoog@gmail.com>
2013-02-24 14:48:07 -0800

    Fixed fblogin.html reference to fblogin.js

--- main.py
+++ main.py
@@ -5,10 +5,6 @@ from google.appengine.ext.webapp import template
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        CONSUMER_KEY = "3jdky3k3ttjtz89dcympbkrm"
-        CONSUMER_SECRET = "D438GSs8gK"
-        CALLBACK_URL = '{}player'.format(self.request.url)
-
         self.response.headers['Content-Type'] = 'text/html'
         path = os.path.join(os.path.dirname(__file__), 'master.html')
         self.response.out.write(template.render(path, {}))
