43cb21c424ac6611f072b93f3e2e1e4a447af8f2
Nick Van Hoogenstyn <nickvanhoog@gmail.com>
2013-02-24 19:27:45 -0800

    Removed Rdio stuff

--- js/master.js
+++ js/master.js
@@ -12,7 +12,7 @@ window.fbAsyncInit = function() {
             //not logged in upon opening the page
             if (response.status === 'connected') {
                 console.log("CONNECTED");
-                window.location = "player";
+                window.location = "rdiologin";
             } 
             else {
                 console.log("NOT CONNECTED");
